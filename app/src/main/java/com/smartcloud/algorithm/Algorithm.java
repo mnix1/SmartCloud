861fa93e648c373457faef90edccaab0eae1ced2
MNIX <mvppl123@gmail.com>
2016-06-05 02:06:23 +0200

    Fix

--- app/src/main/java/com/smartcloud/algorithm/Algorithm.java
+++ app/src/main/java/com/smartcloud/algorithm/Algorithm.java
@@ -5,7 +5,7 @@ public enum Algorithm {
     FIXED__RANDOM_WITH_MEM("", "Stały, nie większy niż wybrany", "Losowa z pamięcią"),
     RANDOM__RANDOM_WITHOUT_MEM("", "Losowy", "Losowa bez pamięci"),
     RANDOM__RANDOM_WITH_MEM("", "Losowy", "Losowa z pamięcią"),
-    FIXED_BY_FILE_SIZE__EVERY("", "Stały, zależny od rozmiaru pliku: ROZMIAR PLIKU / ILOSC MASZYN", "Każda maszyna"),
+    FIXED_BY_FILE_SIZE__EVERY("", "Stały, zależny od rozmiaru pliku: ROZMIAR PLIKU / ILOŚĆ MASZYN", "Każda maszyna"),
     BY_CAPACITY_AND_FILE_SIZE__EVERY("", "Zmienny, zależny od pojemności i rozmiaru pliku: ROZMIAR PLIKU  * POJEMNOŚĆ MASZYNY / POJEMNOŚĆ CHMURY", "Każda maszyna"),
     BY_CAPACITY__BY_CAPACITY("", "Zmienny, zależny od pojemności: POJEMNOŚĆ MASZYNY", "Według pojemności (od największej dostępnej)");
     public static int[] sizes = new int[]{64, 128, 256, 512, 1024, 2048, 4096, 8192};
